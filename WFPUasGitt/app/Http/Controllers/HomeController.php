e2557d200e68ce1393adae278175fb83f0184162
Ewing <luigi.heroes@gmail.com>
2017-12-04 20:35:44 +0700

    kelupaan dan atur matakuliah biar bisa diliat tanpa login

--- WFPUasGitt/app/Http/Controllers/HomeController.php
+++ WFPUasGitt/app/Http/Controllers/HomeController.php
@@ -26,4 +26,9 @@ class HomeController extends Controller
     {
         $this->middleware('auth');
     }
+
+    public function pagenotfound()
+    {
+        return view('content.pagenotfound');
+    }
 }
